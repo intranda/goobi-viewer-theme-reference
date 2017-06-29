be7c469cd7a878e741c975db6aa287bbacc420cc
Marc Lettau-Poelchen <marc.lettau-poelchen@intranda.com>
2017-06-29 09:42:26 +0200

    improvements for tinymce init

--- goobi-viewer-theme-reference/src/META-INF/resources/resources/themes/reference2/javascript/dev/custom.js
+++ goobi-viewer-theme-reference/src/META-INF/resources/resources/themes/reference2/javascript/dev/custom.js
@@ -9,22 +9,4 @@ $( document ).ready( function() {
     };
     
     viewerJS.init( viewerConfig );
-    
-    // TODO: Für ZLB einbauen
-    // var customColorMap = [
-    // "FFFFFF", "ZLB-Weiß",
-    // "333333", "ZLB-Schwarz",
-    // "dedede", "ZLB-Hellgrau",
-    // "727c87", "ZLB-Mittelgrau",
-    // "9a9a9a", "ZLB-Dunkelgrau",
-    // "CD0000", "ZLB-Rot",
-    // "92406d", "ZLB-Lila",
-    // "6f2c40", "ZLB-Bordeaux",
-    // "ffa100", "ZLB-Orange",
-    // "669933", "ZLB-Grün",
-    // "3e5d1e", "ZLB-Dunkelgrün",
-    // "a9d0f5", "ZLB-Hellblau",
-    // "28779f", "ZLB-Blau"
-    // ];
-    // _tinyConfig.textcolor_map =
 } );
