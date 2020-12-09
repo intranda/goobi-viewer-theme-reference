d85610557366b66de1eb08d8ffe5ad8e996f339b
Jan Günther <jan.guenther@intranda.com>
2020-12-09 18:06:15 +0100

    initial commit refs #19937

---
+++ goobi-viewer-theme-sbnue/Gruntfile.js
@@ -0,0 +1,115 @@
+const fs = require("fs")
+let homedir = require("os").homedir();
+let rawdata = fs.readFileSync(homedir + '/.config/grunt_userconfig.json');
+let config = JSON.parse(rawdata);
+
+function getTomcatDir() {
+	return config.tomcat_dir;
+}
+
+module.exports = function(grunt) {
+	// ---------- PROJECT CONFIG ----------
+    grunt.initConfig({
+        theme: {
+            name: 'sbnue',
+            subThemeOne: 'subtheme1',
+            subThemeTwo: 'subtheme2' 
+        },
+        pkg: grunt.file.readJSON('package.json'),
+        src: {
+        	jsDevFolder: 'WebContent/resources/themes/<%=theme.name%>/javascript/dev/',
+            jsDistFolder: 'WebContent/resources/themes/<%=theme.name%>/javascript/dist/',
+            lessDevCsFolder: 'WebContent/resources/themes/<%=theme.name%>/css/less/crowdsourcing/',
+            lessDevViewerFolder: 'WebContent/resources/themes/<%=theme.name%>/css/less/',
+            lessSubThemeOneFolder: 'WebContent/resources/themes/<%=theme.name%>/css/less/subthemes/<%=theme.subThemeOne%>/',
+            lessSubThemeTwoFolder: 'WebContent/resources/themes/<%=theme.name%>/css/less/subthemes/<%=theme.subThemeTwo%>/',
+            cssDistFolder: 'WebContent/resources/themes/<%=theme.name%>/css/dist/'
+        },
+        sync: {
+        	main: {
+        		files: [{
+        			cwd: 'WebContent',
+        			src: [
+        				'**'
+        			],
+        			dest: getTomcatDir() + '\\goobi-viewer-theme-sbnue'
+        		}],
+        		verbose: true
+        	}
+        },
+        less: {
+            development: {
+                options: {
+                    paths: [ '<%=src.lessDevViewerFolder%>', '<%=src.lessDevCsFolder%>' ],
+                    plugins: [
+                        new ( require( 'less-plugin-autoprefix' ) ) ( { browsers: ["last 2 versions"] } )
+                    ],
+                    compress: false,
+                    optimization: 9
+                }
+            },
+            production: {
+                options: {
+                    paths: [ '<%=src.lessDevViewerFolder%>', '<%=src.lessDevCsFolder%>'  ],
+                    plugins: [
+                        new ( require( 'less-plugin-autoprefix' ) ) ( { browsers: ["last 2 versions"] } )
+                    ],
+                    compress: true,
+                    sourceMap: true,
+                    banner: '/*!\n ============================================================\n <%= pkg.name %> \n\n Version: <%= pkg.version %> \n License: <%= pkg.license %> \n Author: <%= pkg.author %> \n Description: <%= pkg.description %> \n ============================================================ \n*/',
+                },
+                files: {
+                    '<%=src.cssDistFolder %><%=theme.name%>.min.css': '<%=src.lessDevViewerFolder%>constructor.less',
+                    '<%=src.cssDistFolder %><%=theme.name%>-cs.min.css': '<%=src.lessDevCsFolder%>csConstructor.less',
+                	'<%=src.cssDistFolder %><%=theme.subThemeOne%>.min.css': '<%=src.lessSubThemeOneFolder%>subThemeConstructor.less',
+                	'<%=src.cssDistFolder %><%=theme.subThemeTwo%>.min.css': '<%=src.lessSubThemeTwoFolder%>subThemeConstructor.less',
+                }
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    '<%=src.jsDistFolder %>custom.min.js': ['<%=src.jsDevFolder %>custom.js']
+                }
+            }
+        },
+        watch: {
+        	configFiles : {
+				files : [ 'Gruntfile.js' ],
+				options : {
+					reload : true
+				}
+			},
+            css: {
+                files: [ 'WebContent/resources/themes/<%=theme.name%>/css/**/*.less' ],
+                tasks: [ 'less', 'sync' ],
+                options: {
+                    spawn: false,
+                }
+            },
+            scripts: {
+                files: [ '<%=src.jsDevFolder %>*.js' ],
+                tasks: [ 'uglify', 'sync' ],
+                options: {
+                	spawn: false,
+                }
+            },
+            html: {
+            	files: [ 'WebContent/resources/themes/<%=theme.name%>/**/*html' ],
+                tasks: [ 'sync' ],
+                options: {
+                	spawn: false,
+                }
+            }
+        }
+    });
+    
+	// ---------- LOAD TASKS ----------
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-sync');
+    
+	// ---------- REGISTER DEVELOPMENT TASKS ----------
+    grunt.registerTask( 'default', [ 'watch', 'sync' ] );
+};
---
+++ goobi-viewer-theme-sbnue/WebContent/resources/themes/sbnue/README.md
@@ -0,0 +1,16 @@
+# README
+
+**Kunde**: intranda GmbH\
+**Goobi viewer Version**: 3.4\
+**Boilerplate Version**: 3.4.0\
+**Goobi viewer Theme**: sbnue
+
+## Besonderheiten
+
+## Überschriebene Seiten und Komponenten
+
+Name | Grund
+------------ | -------------
+myComponent | myReason
+
+## Sonstiges
