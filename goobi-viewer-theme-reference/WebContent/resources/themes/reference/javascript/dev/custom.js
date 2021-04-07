982089890fc477fec73b817c5fc22ecb540837e2
Jan Günther <jan.guenther@intranda.com>
2021-04-07 15:28:52 +0200

    comment for language toggle faces config, theme version

--- goobi-viewer-theme-reference/WebContent/resources/themes/reference/javascript/dev/custom.js
+++ goobi-viewer-theme-reference/WebContent/resources/themes/reference/javascript/dev/custom.js
@@ -34,7 +34,6 @@ function initSliders() {
         //    }
         // }
 	// viewerJS.slideshows.update('styleName', partialConfig);
-
         // add new swiper config
         // var config = {
         //    swiperConfig: {
